89058df56c874dc9557b8d055dc37d71c1b172ad
sambo2021 <mohamedsambomahmoud@gmail.com>
2023-12-26 01:01:05 +0200

    deploying istio-istiod by argocd

---
+++ charts/istio-istiod-test/generate-chart/helm_render.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e -o pipefail
+chart_version="1.20.1"
+repo_name="istio"
+chart_name="istiod"
+repo_url="https://istio-release.storage.googleapis.com/charts/"
+release_name="istiod"
+helm repo add --force-update ${repo_name} ${repo_url}
+helm pull ${repo_name}/${chart_name} --version ${chart_version} --untar --untardir ../
+
+## i made it as script instead of doing these commands
+## in future maybe we need to override some values 
+## namespace by default for this chart is istio-system
+# helm repo add istio https://istio-release.storage.googleapis.com/charts
+# helm install istiod istio/istiod -n istio-system --wait
