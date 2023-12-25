8db2393d7e07df675eee279f5b33a75ede485271
sambo2021 <mohamedsambomahmoud@gmail.com>
2023-12-25 15:48:20 +0200

    delete root app

--- charts/argocd-test/generate-chart/helm_render.sh
+++ charts/argocd-test/generate-chart/helm_render.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e -o pipefail
-chart_version="5.46.8"
+chart_version="5.50.0"
 repo_name="argo-helm"
 chart_name="argo-cd"
 repo_url="https://argoproj.github.io/argo-helm"
@@ -9,7 +9,7 @@ helm repo add --force-update ${repo_name} ${repo_url}
 helm pull ${repo_name}/${chart_name} --version ${chart_version} --untar --untardir ../
 ##dont need to delete redis-ha chart since the default installation
 ##without it as mentioned in docs https://github.com/argoproj/argo-helm/tree/main/charts/argo-cd
-#rm -rf ../${chart_name}/charts
+rm -rf ../${chart_name}/charts
 rm ../${chart_name}/Chart.lock
 yq m -x ../${chart_name}/values.yaml ./values-default.yaml > ./values.yaml
 cat ./values.yaml > ../${chart_name}/values.yaml
