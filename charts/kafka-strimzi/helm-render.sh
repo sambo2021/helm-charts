6a820b78ef47fd979965c7840814fbca393aa0e7
sambo2021 <mohamedsambomahmoud@gmail.com>
2025-11-25 19:14:21 +0200

    add namespaced rules fr strimzi kafka

--- charts/kafka-strimzi/helm-render.sh
+++ charts/kafka-strimzi/helm-render.sh
@@ -5,7 +5,4 @@ repo_name="oci://quay.io/strimzi-helm/strimzi-kafka-operator"
 rm -rf ./strimzi-kafka-operator
 helm pull ${repo_name} --version ${chart_version} --untar --untardir .
 rm -rf ./rendered
-helm template strimzi-kafka-operator ./strimzi-kafka-operator --output-dir ./rendered \
-  --set rbac.create=false \
-  --set createGlobalResources=false \
-  --set watchNamespaces={kafka}
+helm template strimzi-kafka-operator ./strimzi-kafka-operator --output-dir ./rendered --set rbac.create=false   --set createGlobalResources=false   --set watchNamespaces={kafka}
