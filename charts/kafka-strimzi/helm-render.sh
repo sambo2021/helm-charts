d4475b2257c8c10f74bdd93cdebef0041f2521af
sambo2021 <mohamedsambomahmoud@gmail.com>
2025-11-25 17:07:32 +0200

    deploy kafka operator

---
+++ charts/kafka-strimzi/helm-render.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e -o pipefail
+chart_version="0.49.0"
+repo_name="oci://quay.io/strimzi-helm/strimzi-kafka-operator"
+rm -rf ./strimzi-kafka-operator
+helm pull ${repo_name} --version ${chart_version} --untar --untardir .
+rm -rf ./rendered
+helm template strimzi-kafka-operator ./strimzi-kafka-operator --output-dir ./rendered \
+  --set rbac.create=false \
+  --set createGlobalResources=false \
+  --set watchNamespaces={kafka}
---
+++ charts/minio/helm_render.sh
@@ -0,0 +1,20 @@
+# #!/usr/bin/env bash
+# set -e -o pipefail
+# chart_version="1.28.0"
+# repo_name="istio"
+# chart_name="istiod"
+# repo_url="https://istio-release.storage.googleapis.com/charts/"
+# release_name="istiod"
+# helm repo add --force-update ${repo_name} ${repo_url}
+# rm -rf ./${chart_name}
+# helm pull ${repo_name}/${chart_name} --version ${chart_version} --untar --untardir .
+
+# ## i made it as script instead of doing these commands
+# ## in future maybe we need to override some values 
+# ## namespace by default for this chart is istio-system
+# # helm repo add istio https://istio-release.storage.googleapis.com/charts
+# # helm install istiod istio/istiod -n istio-system --wait
+
+
+curl -O https://raw.githubusercontent.com/minio/operator/master/helm-releases/operator-4.3.6.tgz
+tar -zxf operator-4.3.6.tgz 
